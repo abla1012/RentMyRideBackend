a4750f4d1081378b14dda61f804f6aedc8ce5ae0
LA <abla1012@h-ka.de>
2023-06-12 14:57:04 +0200

    Backend

--- src/main/kotlin/com/acme/rentmyride/datasource/mock/MockFahrzeugDataSource.kt
+++ src/main/kotlin/com/acme/rentmyride/datasource/mock/MockFahrzeugDataSource.kt
@@ -34,7 +34,7 @@ class MockFahrzeugDataSource : FahrzeugDataSource {
             ausstattung = "sehr gut",
             zeitraum = "30.06.2023 - 14.07.2023",
             fotoURL = encodedString,
-            id = 0
+            id = 5
         ),
         Fahrzeug(
             marke = "Porsche",
